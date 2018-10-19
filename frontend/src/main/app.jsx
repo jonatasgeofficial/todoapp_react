483b740d4e7d2645b5f643419937842e27bfda9a
jonatasgeofficial <jonatasge@gmail.com>
2018-10-19 13:35:42 -0300

    Frontend - Components added: Todo and About

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,12 @@ import 'modules/materialize-css/dist/css/materialize.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about'
 
 export default props => (
     <div className="row">
-        <h1>Hello World</h1>
+        <Todo />
+        <About />
     </div>
 )
---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    render(props) {
+        return (
+            <div className="row">
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
