7ca43bb0fc981dcf7cfea3988d93ed6a5dcc819d
jonatasgeofficial <jonatasge@gmail.com>
2018-10-19 13:27:59 -0300

    Frontend - Add index.jsx and app.jsx, the first component: Hello World

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,10 @@
+import 'modules/materialize-css/dist/css/materialize.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+export default props => (
+    <div className="row">
+        <h1>Hello World</h1>
+    </div>
+)
